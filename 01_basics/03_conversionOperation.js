0025321fd988ae2d1c80787e8d3683a4fea31022
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-13 09:54:07 +0000

    Datatype Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score ="histesh"
+
+console.log(typeof score);
+console.log(typeof(score)); //both are same
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" -> 33
+//"33abc" -> NaN
+//true -> 1; false -> 0
+
+let isLogged ="hitesh"
+
+let booleanIsLogged = Boolean(isLogged)
+
+console.log(booleanIsLogged);
+
+//1-> true; 0-> false
+//""-> false
+//"hitesh"-> true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

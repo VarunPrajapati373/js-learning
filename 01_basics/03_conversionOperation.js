fb429dd35312bf1a65d327ca0f70e9589963d6aa
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-15 17:41:02 +0000

    03_Conversion topic

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score ="histesh"
 
-console.log(typeof score);
-console.log(typeof(score)); //both are same
+// console.log(typeof score);
+// console.log(typeof(score)); //both are same
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" -> 33
 //"33abc" -> NaN
@@ -15,7 +15,7 @@ let isLogged ="hitesh"
 
 let booleanIsLogged = Boolean(isLogged)
 
-console.log(booleanIsLogged);
+//console.log(booleanIsLogged);
 
 //1-> true; 0-> false
 //""-> false
@@ -23,5 +23,41 @@ console.log(booleanIsLogged);
 
 let someNumber =33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/***********************Conversion*********************/
+ let  value = 3
+ let negValue= -(value)
+//  console.log(negValue)
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3); //power 2^3
+//  console.log(2/3); //quotient
+//  console.log(2%3); // remainder
+
+let str1 ="hello"
+let str2 = " varun"
+str3 = str1+str2
+console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122 if String is first then all converts to String
+// console.log(1+2+"2"); //32 //if first and second int then first solve then string
+// console.log(2+"2"+2); //222
+// console.log(2+2+"2"+2);//422
+
+
+// console.log(+true); //1
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter =100
+// ++gameCounter;
+// gameCounter++
+// console.log(gameCounter);

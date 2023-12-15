66fec063e1636442816f9b470f490ede472c1393
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-15 17:51:58 +0000

    comparison done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+// console.log(null >0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true, comparison convert null to a number, treating it as 0.
+// console.log(undefined ==0); //false
+
+//strict check ===
+
+console.log("2"===2);

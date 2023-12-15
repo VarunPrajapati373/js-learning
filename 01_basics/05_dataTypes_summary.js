affc59764465616ea3837df50b74f10005cd1b89
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-15 18:14:59 +0000

    datatype Summary done

---
+++ 01_basics/05_dataTypes_summary.js
@@ -0,0 +1,29 @@
+//# primitive based on call by value, changes in copy of value
+//7 type : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const id = Symbol('123');
+// const AnotherId = Symbol('123');
+
+// console.log(id=== AnotherId);
+
+
+//# non- primitive or Reference type
+//Arrays, Objects, Functions
+
+//const heros = ["shaktiman", "naagraj", "doga"]; //array
+
+//object
+// let myObj= {
+//     name: "varun",
+//     age: 22
+// }
+
+// const myFucntion = function (){
+
+//     console.log("hello world");
+// }
+
+
+// console.log(typeof BigBumver); //undefined
+// console.log(typeof null); //object
+// console.log(typeof undefined); //undefined

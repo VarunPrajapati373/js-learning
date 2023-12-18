667cf51ffcfc0854b8ec237fcfa5a16dc606d592
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-18 13:04:28 +0000

    basic scope done

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,23 @@
+if(true){
+    const username = "Varun"
+    if(username === "Varun"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+
+   // console.log(website);
+}
+
+//console.log(username);
+
+//++++++++++++++++++ Intresting +++++++++++++
+
+function addOne(num){
+    return num +1
+}
+addOne(5)
+
+const addTwo = function(num){ //expression method to use method and store value in a variable
+    return num+1
+}
+

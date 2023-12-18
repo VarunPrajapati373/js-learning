d5cf76e8accc7168f9dadfd7b300ef01909c3c25
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-18 08:33:49 +0000

    Object De-structuring done

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -29,7 +29,7 @@ const obj2 = {3: "c", 4: "d"}
 
 const obj3 = {...obj1, ...obj2}
 
-console.log(obj3);
+//console.log(obj3);
 
 const users = [
     {
@@ -49,11 +49,31 @@ const users = [
 
 users[1].email
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('email'));
-console.log(tinderUser.hasOwnProperty('name'));
+// console.log(tinderUser.hasOwnProperty('email'));
+// console.log(tinderUser.hasOwnProperty('name'));
+
+
+//++++++++++++++++++++ OBJECT DE-Structure ++++++++++++++++++++++++++++++++++++
+
+const course ={
+    courseName : "Js in Hindi",
+    price : "999",
+    courseInstructor : "Hitesh Chaudhary"
+}
+
+//first way to access
+console.log(course.courseInstructor);
+
+//good way to access
+
+const {courseInstructor} = course
+//if you want to give Alias name then,
+const {courseInstructor: instructor} = course
+console.log(courseInstructor);
+console.log(instructor);

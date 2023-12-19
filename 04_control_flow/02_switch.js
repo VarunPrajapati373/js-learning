79831077127e2d4cee1e67c2c9410c86e12952dc
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-19 17:07:03 +0000

    Switch basic done

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,29 @@
+//FALSY Values : false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const weekDay = 2
+
+switch(weekDay){
+    case 1: 
+        console.log("Sunday");
+        break;
+    
+    case 2: 
+        console.log("Monday");
+        break;
+
+    default: 
+        console.log("Rest Days");
+        break;
+
+}

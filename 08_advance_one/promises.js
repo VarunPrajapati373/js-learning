04f96ec9f400c1c774a788630eae1dc4db43f723
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-26 11:29:14 +0000

    commented code

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -1,64 +1,64 @@
-const promiseOne = new Promise(function(resolve, reject){
-    //Do an Async Task
-    //DB calls, network
+// const promiseOne = new Promise(function(resolve, reject){
+//     //Do an Async Task
+//     //DB calls, network
 
-    setTimeout(function(){
-        console.log("Async Tasks are compelete");
-        resolve() //connect resolve and then method
-    },1000)
-})  //create promise
+//     setTimeout(function(){
+//         console.log("Async Tasks are compelete");
+//         resolve() //connect resolve and then method
+//     },1000)
+// })  //create promise
 
 
-//consumption of promise
-promiseOne.then(function(){
-    console.log("Promise Consumed");
-})
+// //consumption of promise
+// promiseOne.then(function(){
+//     console.log("Promise Consumed");
+// })
 
 //++++++++++++++++++++ ANOTHER WAY TO WRITE PROMISE ++++++++++++++++++++++++++++++++
 
-new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async Task - 2");
-        resolve()
-    },1000)
-}).then(function(){
-    console.log("Async Task -2 Completed");
-})
+// new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async Task - 2");
+//         resolve()
+//     },1000)
+// }).then(function(){
+//     console.log("Async Task -2 Completed");
+// })
 
 //++++++++++++++++++ Another promise (Data Consumption) ++++++++++++++++++++++++++++++++++
 
-const promiseThree = new Promise(function(resolve, reject){
-    //Do an Async Task
-    //DB calls, network
+// const promiseThree = new Promise(function(resolve, reject){
+//     //Do an Async Task
+//     //DB calls, network
 
-    setTimeout(function(){
-        resolve({username: "Chai", email:"chai@example.com"}) //connect resolve and then method
-    },1000)
-})  //create promise
+//     setTimeout(function(){
+//         resolve({username: "Chai", email:"chai@example.com"}) //connect resolve and then method
+//     },1000)
+// })  //create promise
 
-//consumption of promise
-promiseThree.then(function(user){
-    console.log(user);
-})
+// //consumption of promise
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
 
 //++++++++++++++++++ Another promise () ++++++++++++++++++++++++++++++++++
 
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error= true
-        if(!error){
-            resolve({username: "Varun", password: "123"})
-        }else{
-            reject('ERROR: Something went wrong')
-        }
-    },1000)
-})
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error= true
+//         if(!error){
+//             resolve({username: "Varun", password: "123"})
+//         }else{
+//             reject('ERROR: Something went wrong')
+//         }
+//     },1000)
+// })
 
-promiseFour.then((user)=>{
-    console.log(user);
-    return user.username
-}).then((username)=>{
-    console.log(username);
-}).catch(function(e){
-    console.log(e);
-}).finally(()=> console.log("The promise is either resolved or rejected"))
+// promiseFour.then((user)=>{
+//     console.log(user);
+//     return user.username
+// }).then((username)=>{
+//     console.log(username);
+// }).catch(function(e){
+//     console.log(e);
+// }).finally(()=> console.log("The promise is either resolved or rejected"))

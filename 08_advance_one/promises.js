94dd0f5ff28c1d069a81d1afa7faf309f7534c3c
Varun Prajapati <65784683+VarunPrajapati373@users.noreply.github.com>
2023-12-27 10:45:03 +0000

    Fetch and Promise Done

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -62,3 +62,57 @@
 // }).catch(function(e){
 //     console.log(e);
 // }).finally(()=> console.log("The promise is either resolved or rejected"))
+
+// ++++++++++++++++++++++++++++ Another intresting promise ++++++++++++++++++++++++++++++++++++
+
+const promiseFive = new Promise(function(resolve, reject){
+
+    setTimeout(function(){
+                let error= false
+                if(!error){
+                    resolve({username: "Javascript", password: "123"})
+                }else{
+                    reject('ERROR: Something went wrong')
+                }
+            },1000)
+
+})
+
+async function consumePromiseFive(){
+   try {
+    const response = await promiseFive
+    console.log(response);
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+consumePromiseFive()
+
+//++++++++++++++++++++++++++++ Another Promise +++++++++++++++++
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log('E: ', error);
+//     }
+// }
+
+// getAllUsers()
+
+//Another method to do the same using thenable method
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((response)=>{
+//     return response.json()
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((error)=> console.log(error))
+
+
---
+++ 09_OOP/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes
+JavaScript is prototype based language. It is not an OO language
+
+# OOP
+
+# Object
+- collection of properties and method
+- to lowerCase, toUpperCase
+
+# why use OOP
+
+# Parts of OOP
+Object Literal
+
+-Constructor Function
+-prototypes
+-classes
+-Instance (new, this)
+
+# 4 Pillars
+Abstraction
+Encapsulation
+Inheritence
+Polymorphism
